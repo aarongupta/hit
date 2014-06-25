8eb1b0282936a7075223f50576f1e270a5c79327
Aaron Gupta <aaronkgupta@gmail.com>
2014-06-25 16:24:46 -0700

    d

--- hit.py
+++ hit.py
@@ -4,13 +4,12 @@ from Bio import Entrez, SeqIO
 from Bio.SeqUtils import GC
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 
-# function to retrieve genbank data from NCBI website
+# function to retrieve genbank data from NCBI website. retrieves accession ID and nucelotide sequence
 def parse(accession, filetype): # filetype can be 'gb' or 'fasta'
-	Entrez.email = "aaronkgupta@gmail.com"     # tells NCBI who you are
-	handle = Entrez.efetch(db="nucleotide", id = accession, rettype = filetype, retmode="text")
-    seq_record = SeqIO.read(handle, 'gb')
-	return seq_record.name, seq_record.seq
+    Entrez.email = "aaronkgupta@gmail.com"
+    handle = Entrez.efetch(db="nucleotide", id = accession, rettype = filetype, retmode="text")
+    seq_record = SeqIO.read(handle,'gb') # genbank format
+    return seq_record.name, seq_record.seq.translate(seq_record.seq.tostring, table = 'Standard', stop_symbol='*')
 
-handle = parse('BC011922','gb')
-
-print seq_record.seq
+handle = parse('BC011922','gb') # sample case
+print handle
