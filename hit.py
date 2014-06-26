288ff8dbec692f2e2d8024826bd4b0b594ed5a66
Aaron <aaronkgupta@gmail.com>
2014-06-26 07:51:52 -0700

    writefile and nar td

--- hit.py
+++ hit.py
@@ -8,30 +8,72 @@ from requests.auth import HTTPBasicAuth
 import xlrd
 import csv
 
+# converts each sheet in ORF.xls to a separate .csv
 def csv_from_excel():
 	wb = xlrd.open_workbook('ORF program.xls')
 	sh = wb.sheet_by_name('ORF')
-	your_csv_file = open('ORF.csv', 'wb')
-	wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
-
+	sh1 = wb.sheet_by_name('RNAseq')
+	c = open('ORF.csv', 'wb')
+	d = open('RNAseq.csv','wb')
+	wr = csv.writer(c, quoting=csv.QUOTE_ALL)
+	wr1 = csv.writer(d, quoting=csv.QUOTE_ALL)
 	for rownum in xrange(sh.nrows):
-		wr.writerow(sh.row_values(rownum))
+		try:
+			wr.writerow(sh.row_values(rownum))
+		except UnicodeEncodeError:
+			pass	
+	for rownum in xrange(sh1.nrows):
+		try:
+			wr1.writerow(sh1.row_values(rownum))
+		except UnicodeEncodeError:
+			pass
+	c.close()
 
-	your_csv_file.close()
+# extracts hit coordinates and corresponding barcodes from ORF sheet
+def get_hits(csvfile):
+	hits = []
+	barcodes = []
+	with open(csvfile, 'rU') as file:
+		reader = csv.reader(file)
+		arr = []
+		for row in reader:
+			if row[3] != "":
+				hits.append(row[3].strip())
+				barcodes.append(row[2].strip())
+	return hits, barcodes
 
-# function to retrieve genbank data from NCBI website. retrieves accession ID and nucelotide sequence
-def parse(accession, filetype): # filetype can be 'gb' or 'fasta'
-    Entrez.email = "aaronkgupta@gmail.com"
-    handle = Entrez.efetch(db="nucleotide", id = accession, rettype = filetype, retmode="text")
-    seq_record = SeqIO.read(handle,'gb') # genbank format
-    return seq_record.name, seq_record.seq.tostring
-    
+# gets NCBI accession number from NAR website
 def get_id(x,y,barcode):
 	r = requests.get('http://research.gene.com/nar',auth=HTTPBasicAuth('guptaa22','Scissor1')).text
 	#find tag in html and return accession
 	return accession
 
-csv_from_excel()
+# retrieves GenBsank data from NCBI website: accession ID, protein seq, and gene name
+def parse(accession, filetype): # filetype can be 'gb' or 'fasta'
+    Entrez.email = "aaronkgupta@gmail.com"
+    handle = Entrez.efetch(db="nucleotide", id = accession, rettype = filetype, retmode="text")
+    seq_record = SeqIO.read(handle,'gb') # genbank format
+    feats = seq_record.features
+    name = []
+    for feat in feats:
+    	if 'gene' in feat.qualifiers:
+    		name.append(feat.qualifiers['gene'])
+    return seq_record.name, str(seq_record.seq.translate()), name[0]
+
+# gets RNAseq data from RNAseq sheet
+def get_rnaseq(rnaseq):
+	
+    
+# write output file which includes GenBank data
+def writefile(orf, rnaseq, name, seq, symbol):
+	# add genbank data to ORF sheet
+	with open(orf, 'rU') as file:
+		reader = csv.reader(file)
+		arr = []
+		for row in reader:
+			
+#csv_from_excel()
+#get_hits('ORF.csv')
 
 #get_id(0,0,0)
 #handle = parse('BC011922','gb') # sample case
